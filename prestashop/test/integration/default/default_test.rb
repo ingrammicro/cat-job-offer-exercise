2de29a4ba953a908014c798cd568dd677f8007cf
MiguiTE <miguite19@gmail.com>
2019-07-09 21:41:06 +0200

    Tests asked in part 2

--- prestashop/test/integration/default/default_test.rb
+++ prestashop/test/integration/default/default_test.rb
@@ -4,3 +4,18 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
+
+
+# verify docker instalation
+describe docker.version do
+    its('Server.Version') { should cmp >= '0.0'}
+    its('Client.Version') { should cmp >= '0.0'}
+end
+
+
+
+# check instalation of prestashop and listening port
+describe docker_container(name: 'my_prestashop') do
+    it { should exist }
+    its('ports') {should eq '0.0.0.0:80->80/tcp'}
+  end
