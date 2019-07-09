b519f10c3af9b6135b77932bbba89824984c0eb9
MiguiTE <miguite19@gmail.com>
2019-07-09 20:36:40 +0200

    Finish activities 1-3

--- prestashop/metadata.rb
+++ prestashop/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures prestashop'
 long_description 'Installs/Configures prestashop'
 version '0.1.0'
 chef_version '>= 13.0'
-
+depends 'docker'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- prestashop/recipes/default.rb
+++ prestashop/recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+
+# Install docker with docker cookbooks
+docker_installation 'default'
+
+# pull prestashop image
+docker_image "prestashop/prestashop" do
+    tag 'latest'
+    action :pull
+end
+
+# Create and run prestashop container
+docker_container "my_prestashop" do
+    repo 'prestashop/prestashop'
+    tag 'latest'
+    port '80:80'
+    action :run
+end
