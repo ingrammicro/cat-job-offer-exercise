bad7e832427a770d844098f87ff74b677701ab6b
Bryan Ferreira <bryandfh@gmail.com>
2019-07-13 01:21:24 +0200

    Docker cookbooks gems added to recipe

--- prestashop/recipes/default.rb
+++ prestashop/recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+#For docker usage: https://github.com/chef-cookbooks/docker
+docker_installation 'default'
+
+docker_service 'default' do
+  action [:create, :start]
+end
+
+#Webpage for Docker prestashop: https://hub.docker.com/r/prestashop/prestashop/
+docker_image "prestashop/prestashop" do
+    action :pull
+end
+
+docker_container "my_prestashop" do
+    repo 'prestashop/prestashop'
+    port '80:80'
+    action :run
+end 
